01a10a02b6e94f714a3993fe2cf13490e9a746b1
PatriciaDivi <patriciavianad@gmail.com>
2021-02-11 17:00:52 -0300

    1commit - Projeto ChurrasComentro

---
+++ churrasCometro/scripts/script.js
@@ -0,0 +1,62 @@
+// Carne - 400 gr por pessoa + de 6 horas - 650
+// Cerveja - 1200 ml por pessoa + de 6 horas - 2000 ml
+// Refrigerante/ água - 1000 ml por pessoa + de 6 horas 1500ml
+
+// Crianças valem por 0,5
+
+let inputAdultos = document.getElementById("adultos")
+let inputCriancas = document.getElementById("criancas")
+let inputDuracao = document.getElementById("duracao")
+
+let resultado = document.getElementById("resultado")
+
+function calcular() {
+    console.log("calculando...")
+
+    let adultos = inputAdultos.value
+    let criancas = inputCriancas.value
+    let duracao = inputDuracao.value
+
+    let qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) /2 * criancas)
+
+    let qdtTotalCerveja = cervejaPP(duracao) * adultos
+
+    let qdtTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) /2 * criancas)
+
+    resultado.innerHTML = `<p>${qdtTotalCarne / 1000}Kg de Carne</p>`
+
+    resultado.innerHTML += `<p>${Math.ceil(qdtTotalCerveja / 355)} Lata de Cerveja</p>`
+
+    resultado.innerHTML += `<p>${Math.ceil(qdtTotalBebidas / 2000)} Pet's de 2L de Bedidas</p>`
+
+}
+
+function carnePP(duracao) {
+    if (duracao >= 6) {
+        return 650
+    } else {
+        return 400
+
+    }
+
+}
+
+function cervejaPP(duracao) {
+    if (duracao >= 6) {
+        return 2000
+    } else {
+        return 1200
+
+    }
+
+}
+
+function bebidasPP(duracao) {
+    if (duracao >= 6) {
+        return 1500
+    } else {
+        return 1000
+
+    }
+
+}
